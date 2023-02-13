ade709f6253ce25af2b5ced48242b95ad906e6c4
Prashant-1695 <prashant33968@gmail.com>
2023-02-13 11:25:27 +0530

    lavender: Update BoardConfig.mk
    * change defconfig
    * switch to nexus-clang
    * disable llvm_binutils
    
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,8 +50,11 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_DTBOIMG_PARTITION_SIZE := 8388608
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/xiaomi/lavender
-TARGET_KERNEL_CONFIG := lavender-perf_defconfig
+TARGET_KERNEL_CONFIG := lavender_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_LLVM_BINUTILS := false
+TARGET_KERNEL_CLANG_VERSION := nexus
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-nexus
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
