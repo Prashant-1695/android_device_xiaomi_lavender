45ef915b67e00f9ac7f9e7e48744986193d095fd
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2023-06-07 17:02:12 +0530

    Revert "lavender: switch to nexus clang"

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,8 +53,6 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/lavender
 TARGET_KERNEL_CONFIG := lavender_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_LLVM_BINUTILS := false
-TARGET_KERNEL_CLANG_VERSION := nexus
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-nexus
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
