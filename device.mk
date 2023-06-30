31350690509d1605b95744eebb319322a9830d84
Michael Bestas <mkbestas@gmail.com>
2023-06-30 19:24:39 +0530

    lavender: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf
    Signed-off-by: Prashant-1695 <prashant33968@gmail.com>

--- device.mk
+++ device.mk
@@ -287,6 +287,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.lavender
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
