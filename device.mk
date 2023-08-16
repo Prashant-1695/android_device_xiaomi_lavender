f44790c4723e6d443811416b203a31192160c8c2
Prashant-1695 <prashant33968@gmail.com>
2023-08-16 16:16:14 +0530

    lavender: Update dirac
    Signed-off-by: Prashant-1695 <prashant33968@gmail.com>

--- device.mk
+++ device.mk
@@ -150,6 +150,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+# Device-specific Settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Display
 PRODUCT_PACKAGES += \
     gralloc.sdm660 \
